36f8e04843dd46cc786f5370cef04d3e56c508c1
Rajesh <rajeshranjan8271@gmail.com>
2024-12-03 04:05:01 +0530

    Email and Password inputs

--- firebase/src/App.jsx
+++ firebase/src/App.jsx
@@ -1,11 +1,18 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { useAuth } from "./contexts/authContext/auth";
+import { doSignInWithEmailAndPassword } from "./firebase/auth";
 
 function App() {
-  const { userLoggedIn } = useAuth();
-  console.log(user);
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+    console.log(e);
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+    const user = doSignInWithEmailAndPassword(email, password);
+    console.log("User : ", user);
+  };
 
   return (
     <>
@@ -17,43 +24,33 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + Auth0 (OAuth2) Authentication</h1>
+      <h1>Vite + React + Firebase Authentication</h1>
+      <form autoComplete="off" onSubmit={handleFormSubmit}>
+        <input name="email" type="text" placeholder="Enter email..." required />
+        <input
+          name="password"
+          type="password"
+          placeholder="Enter password..."
+          required
+        />
+        <button>Login</button>
+      </form>
       <div className="card">
-        {!isAuthenticated ? (
-          <button onClick={() => loginWithRedirect()}>Login</button>
-        ) : (
-          <button
-            onClick={() =>
-              logout({ logoutParams: { returnTo: window.location.origin } })
-            }
-          >
-            Log Out
-          </button>
-        )}
         <br />
         <br />
-        {isAuthenticated && (
-          <div>
-            <img
-              src={user.picture}
-              alt="Profile Picture"
-              style={{ borderRadius: "100%" }}
-            />
-            <p>
-              <b>Username : </b>
-              {user.name}
-            </p>
-          </div>
-        )}
-      </div>
-      {isAuthenticated && (
         <div>
           <p>
-            <b>Email : </b>
-            {user.email}
+            <b>Username : </b>
+            User name will appear here.
           </p>
         </div>
-      )}
+      </div>
+      <div>
+        <p>
+          <b>Email : </b>
+          User's email will appear here.
+        </p>
+      </div>
     </>
   );
 }
